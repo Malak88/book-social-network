ae3261fcd08a55491c60f7890409e2a2fcb58fbf
malak_bendalla <malak.bendalla22@gmail.com>
2024-09-18 00:40:59 +0100

    app Config / security / user registration + authentication /

--- book-network/src/main/java/com/malak/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/malak/book/BookNetworkApiApplication.java
@@ -1,8 +1,13 @@
 package com.malak.book;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.malak.book.role.Role;
+import com.malak.book.role.RoleRepository;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +17,12 @@ public class BookNetworkApiApplication {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
